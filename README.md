911742b2bf484a058712d9bad06dd57305f92edc
Elena Mischenko <emischenko@justcoded.com>
2016-11-15 18:35:01 +0200

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Skeleton
+
+## Usage
+1. `npm install`
+2. run `webpack-dev-server`
+3. Go to `localhost:8080`
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    console.log('js')
+    return(
+      <div>
+        <h1>React App</h1>
+      </div>
+    );
+  }
+}
